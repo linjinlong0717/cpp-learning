6efeefe62c7426ee1f4ed16dc54cbc09274f87a3
linjinlong0717 <2269514780@qq.com>
2026-05-07 22:44:49 +0800

    新增案例2-制作饮品

---
+++ 03-类和对象/2.5案例2-制作饮品.cpp
@@ -0,0 +1,88 @@
+多态案例二  制作饮品
+一、案例描述：
+制作饮品的大致流程为：煮水 - 冲泡 - 倒入杯中 - 加入辅料
+
+利用多态技术实现本案例，提供抽象制作饮品基类，提供子类制作咖啡和茶叶
+
+二、代码实现：
+class AbstractDrinking
+{
+public:
+	//煮水
+	virtual void Boil() = 0;
+	//冲泡
+	virtual void Brew() = 0;
+	//倒入杯中
+	virtual void PourInCup() = 0;
+	//加入辅料
+	virtual void PutSomething() = 0;
+    //制作饮品
+	void makeDrink()
+	{
+		 Boil();
+		 Brew();
+		 PourInCup();
+		 PutSomething();
+	}
+};
+//制作咖啡
+class Coffee : public AbstractDrinking
+{
+public:
+	virtual void Boil() 
+	{
+		cout << "煮农夫山泉" << endl;
+	}
+	virtual void Brew()
+	{
+		cout << "冲泡咖啡" << endl;
+	}
+	virtual void PourInCup() 
+	{
+		cout << "倒入杯中" << endl;
+	}
+	virtual void PutSomething() 
+	{
+		cout << "加入牛奶和糖" << endl;
+	}
+};
+class Tea : public AbstractDrinking
+{
+public:
+	virtual void Boil()
+	{
+		cout << "煮矿泉水" << endl;
+	}
+	virtual void Brew()
+	{
+		cout << "冲泡茶叶" << endl;
+	}
+	virtual void PourInCup()
+	{
+		cout << "倒入杯中" << endl;
+	}
+	virtual void PutSomething()
+	{
+		cout << "加入枸杞" << endl;
+	}
+};
+//制作函数
+void doWork(AbstractDrinking* abs) //AbstractDrinking *abs=new Coffee
+{
+	abs->makeDrink();
+	delete abs; //释放
+}
+void test01()
+{
+	//制作咖啡
+	doWork(new Coffee);
+
+	cout << "-------------" << endl;
+	//制作茶叶
+	doWork(new Tea);
+}
+int main()
+{
+	test01();
+	return 0;
+}
