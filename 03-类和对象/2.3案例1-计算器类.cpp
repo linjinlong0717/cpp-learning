c4368e628f34709796ff97c804d73eb18ae60c3c
linjinlong0717 <2269514780@qq.com>
2026-05-07 21:40:29 +0800

    新增计算器类

---
+++ 03-类和对象/2.3案例1-计算器类.cpp
@@ -0,0 +1,127 @@
+多态案例--计算器类
+一、案例描述：
+分别利用普通写法和多态技术，设计实现两个操作数进行运算的计算器类
+
+多态的优点：
+·代码组织结构清晰
+·可读性强
+·利于前期和后期的扩展以及维护
+
+二、代码演示：
+//普通写法
+class Calculator
+{
+public:
+	int getResult(string oper)
+	{
+		if (oper == "+")
+		{
+			return m_Num1 + m_Num2;
+		}
+		else if (oper == "-")
+		{
+			return m_Num1 - m_Num2;
+		}
+		else if (oper == "*")
+		{
+			return m_Num1 * m_Num2;
+		}
+		//若想扩展新的功能，需要修改源码
+		
+		//在真实开发中，提倡 开闭原则
+		//开闭原则：对扩展进行开放，对修改进行关闭
+	    //C++开发提倡利用多态设计程序架构，因为多态优点很多
+	}
+	int m_Num1;//操作数1
+	int m_Num2;//操作数2
+};
+void test01()
+{
+	Calculator c;
+	c.m_Num1 = 10;
+	c.m_Num2 = 10;
+	cout << c.m_Num1 << " + " << c.m_Num2 << " = " << c.getResult("+") << endl;
+	cout << c.m_Num1 << " - " << c.m_Num2 << " = " << c.getResult("-") << endl;
+	cout << c.m_Num1 << " * " << c.m_Num2 << " = " << c.getResult("*") << endl;
+}
+int main()
+{
+	test01();
+	return 0;
+}
+
+//利用多态实现计算器
+
+//实现计算器抽象类
+class AbstractCalculator
+{
+public:
+	virtual int getResult()
+	{
+		return 0;
+	}
+	int m_Num1;
+	int m_Num2;
+};
+//加法计算器类
+class AddCalculator :public AbstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 + m_Num2;
+	}
+};
+//减法计算器类
+class SubCalculator :public AbstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 - m_Num2;
+	}
+};
+//乘法计算器类
+class MulCalculator :public AbstractCalculator
+{
+public:
+	int getResult()
+	{
+		return m_Num1 * m_Num2;
+	}
+};
+void test01()
+{
+	//多态使用条件
+	//父类指针或者引用指向子类对象
+
+	//加法运算
+	AbstractCalculator *abc=new AddCalculator;
+	abc->m_Num1 = 10;
+	abc->m_Num2 = 10;
+
+	cout << abc->m_Num1 << " + " << abc->m_Num2 << " = " << abc->getResult() << endl;
+	//用完后记得销毁
+	delete abc;
+	
+	//减法运算
+	abc = new SubCalculator;
+	abc->m_Num1 = 100;
+	abc->m_Num2 = 100;
+
+	cout << abc->m_Num1 << " - " << abc->m_Num2 << " = " << abc->getResult() << endl;
+	delete abc;
+
+	//乘法运算
+    abc = new MulCalculator;
+	abc->m_Num1 = 100;
+	abc->m_Num2 = 100;
+
+	cout << abc->m_Num1 << " * " << abc->m_Num2 << " = " << abc->getResult() << endl;
+	delete abc;
+}
+int main()
+{
+	test01();
+	return 0;
+}
