34710d058e11b780d461632017b0d7b5f0e690dd
linjinlong0717 <2269514780@qq.com>
2026-05-20 20:15:23 +0800

    新增字符串存取

---
+++ 06-STL/2.6string容器-字符串存取.cpp
@@ -0,0 +1,35 @@
+string字符存取
+//string中单个字符存取方式有两种
+·char& operator[](int n); //通过[]方式取字符
+·char& at(int n)         //通过at方式获取字符
+
+代码演示：
+void test01()
+{
+	string str = "hello";
+	//1.通过[]访问单个字符
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << str[i] << " ";
+	}
+	cout << endl;
+
+	//2.通过at方式访问单个字符
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << str.at(i) << " ";
+	}
+	cout << endl;
+
+	//修改单个字符
+	str[0] = 'x';
+	cout << "str = " << str << endl;  //输出xello
+
+	str.at(1) = 'x';
+	cout << "str = " << str << endl;  //输出xxllo
+}
+int main()
+{
+	test01();
+	return 0;
+}
