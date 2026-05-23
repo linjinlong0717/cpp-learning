622fa2d297d3fbb124297eddffb20f45bd483e79
linjinlong0717 <2269514780@qq.com>
2026-05-23 18:31:28 +0800

    新增deque容器-数据存取

---
+++ 06-STL/4.5deque容器-数据存取.cpp
@@ -0,0 +1,40 @@
+deque数据存取
+函数原型：
+·at(int idx);  //返回 索引idx所指的数据
+·operator[];   //返回 索引idx所指的数据
+·front();      //返回 容器中第一个数据元素
+·back();       //返回容器中最后一个数据元素
+
+代码演示：
+void test01()
+{
+	deque<int>d;
+	d.push_back(10);
+	d.push_back(20);
+	d.push_back(30);
+	d.push_front(100);
+	d.push_front(200);
+	d.push_front(300);
+
+	//通过[]方式访问元素
+	for (int i = 0; i < d.size(); i++)
+	{
+		cout << d[i] << " ";
+	}
+	cout << endl;
+
+	//通过at方式访问元素
+	for (int i = 0; i < d.size(); i++)
+	{
+		cout << d.at(i) << " ";
+	}
+	cout << endl;
+
+	cout << "第一个元素为：" << d.front() << endl;
+	cout << "最后一个元素为：" << d.back() << endl;
+}
+int main()
+{
+	test01();
+	return 0;
+}
