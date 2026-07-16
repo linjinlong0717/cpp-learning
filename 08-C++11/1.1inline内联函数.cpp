07768ddc3dda918dddc252354a383690c778187e
linjinlong0717 <2269514780@qq.com>
2026-07-16 15:48:42 +0800

    新增C++11语法

---
+++ 08-C++11/1.1inline内联函数.cpp
@@ -0,0 +1,27 @@
+一、inline 的核心作用
+消除普通函数调用的开销
+
+//普通函数调用流程：
+保存现场、参数压栈
+跳转到函数地址执行
+执行完返回、恢复栈
+频繁调用短小函数时，跳转、栈操作的耗时占比很高。
+
+//inline 优化逻辑：
+编译阶段把函数体代码直接复制粘贴到每一处调用位置，没有跳转、没有栈操作，提升运行速度。
+
+二、代码演示：
+#include<iostream>
+using namespace std;
+inline int func(int a, int b)
+{
+	return a + b;
+}
+int main()
+{
+	int num1 = 10;
+	int num2 = 20;
+	int ret = func(num1, num2);
+	cout << ret << endl;
+	return 0;
+}
