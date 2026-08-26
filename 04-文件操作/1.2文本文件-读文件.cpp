b8001fc09cdebbd987a6a0aae8807070c2a4f3a8
linjinlong0717 <2269514780@qq.com>
2026-08-26 15:41:33 +0800

    新增读文件

--- 04-文件操作/1.2文本文件-读文件.cpp
+++ 04-文件操作/1.2文本文件-读文件.cpp
@@ -1,4 +1,4 @@
-读文件
+读文件(从 文件 到 程序)
 //读文件与写文件步骤相似，但是读取方式相对较多
 
 一、步骤
@@ -6,8 +6,8 @@
 #include<fstream>
 
 2.创建流对象
-ifstream ifs;
-
+ifstream ifs;                          //2.和3.步可以合并为 
+                                       //ifstream ifs("文件路径", ios::in); 或 ifstream ifs("文件路径");
 3.打开文件 并 判断文件是否打开成功
 ifs.open("文件路径", 打开方式);
 
@@ -74,3 +74,30 @@ int main()
 	test01();
 	return 0;
 }
+
+//补充：读int类型数据
+写法1：
+int  a, b, c;
+ifs >> a >> b >> c; //若文件里有“10 20 30”，三个变量分别被赋值
+
+写法2：
+int num;
+while (ifs >> num)
+{
+	cout << num << " "; //会读完全部数字
+}
+
+//读取容器中的数据
+// 先写入 vector
+vector<int> v = { 1, 2, 3, 4, 5 };
+ofstream ofs("vec.txt");         
+ofs << v.size() << endl;
+for (int x : v) ofs << x << " ";
+ofs.close();
+
+// 读取 vector
+ifstream ifs("vec.txt");
+int n;
+ifs >> n;
+vector<int> v2(n);
+for (int i = 0; i < n; i++) ifs >> v2[i];
